95b6f48100e1f96d9197a0c63fd2b57922072053
Yoojin Lee <yoojinLee.dev@gmail.com>
2022-06-21 00:01:29 +0900

    Test: Event Entity test 파일 추가

--- src/test/java/com/yoojin/restapiwithspring/event/EventTest.java
+++ src/test/java/com/yoojin/restapiwithspring/event/EventTest.java
@@ -1,7 +1,29 @@
 package com.yoojin.restapiwithspring.event;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class EventTest {
 
+    @Test
+    public void builder() {
+        Event event = Event.builder()
+                .name("SPRING REST API")
+                .description("SPRING REST API with Spring")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean() {
+        Event event = new Event();
+        String name = "Event";
+        String description = "spring";
+        event.setName(name);
+        event.setDescription(description);
+
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
 }
