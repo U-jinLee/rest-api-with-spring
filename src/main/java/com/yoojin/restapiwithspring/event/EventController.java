63095bee9dd5dc735c0a6bde504b6cfe8a9199d7
Yoojin Lee <yoojinLee.dev@gmail.com>
2022-07-28 00:31:38 +0900

    Feat: 이벤트 목록 조회 API 제작

--- src/main/java/com/yoojin/restapiwithspring/event/EventController.java
+++ src/main/java/com/yoojin/restapiwithspring/event/EventController.java
@@ -2,12 +2,17 @@ package com.yoojin.restapiwithspring.event;
 
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +31,14 @@ public class EventController {
     private final ModelMapper modelMapper;
     private final EventValidator eventValidator;
 
-
+    @Transactional(readOnly = true)
+    @GetMapping()
+    public ResponseEntity getEvents(Pageable pageable, PagedResourcesAssembler<Event> assembler) {
+        Page<Event> events = eventRepository.findAll(pageable);
+//        PagedModel<EntityModel<Event>> pagedModel = assembler.toModel(events);
+        return ResponseEntity.status(HttpStatus.OK).body(events);
+    }
+    @Transactional
     @PostMapping("")
     public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
         if(errors.hasErrors()) {
