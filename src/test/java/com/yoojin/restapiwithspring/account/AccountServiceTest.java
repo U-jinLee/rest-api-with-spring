585df9bdfac44d696d35d4b7ba2c5fb2ed54b0f7
Yoojin Lee <yoojinLee.dev@gmail.com>
2023-01-05 22:39:13 +0900

    Feat: 스프링 Security form 인증 확인

--- src/test/java/com/yoojin/restapiwithspring/account/AccountServiceTest.java
+++ src/test/java/com/yoojin/restapiwithspring/account/AccountServiceTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -29,6 +30,8 @@ public class AccountServiceTest {
     AccountService accountService;
     @Autowired
     AccountRepository accountRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
     @Test
     public void findByUserName() {
         //given
@@ -40,13 +43,13 @@ public class AccountServiceTest {
                 .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
                 .build();
 
-        this.accountRepository.save(account);
+        this.accountService.saveAccount(account);
 
         //when
         UserDetailsService userDetailsService = accountService;
         UserDetails userDetails = userDetailsService.loadUserByUsername(email);
         //then
-        assertThat(userDetails.getPassword()).isEqualTo(account.getPassword());
+        assertThat(this.passwordEncoder.matches(password, userDetails.getPassword())).isTrue();
     }
 
     @Test
